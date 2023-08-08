c52240c85ece9aa2e6aebd80fe8ff3881590189e
Zachary Kuchar <zacharyk323@live.com>
2023-08-08 18:52:55 -0400

    Added Greeting route

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require("express");
 
 const app = express();
 
+const check = true;
+
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
+
+app.get("/greeting/:name", (req, res) => {
+  let name = req.params.name;
+  res.render("Greeting", { Name: name });
+});
 
 app.listen("3000", (req, res) => {
   console.log("Listening on port 3000");
---
+++ views/Greeting.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Greeting(props) {
+  return (
+    <div>
+      <h1>Hello there, {props.Name}! How are you today?!</h1>
+    </div>
+  )
+}
+
+export default Greeting
